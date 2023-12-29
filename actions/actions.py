9d6d3da76141a64b0d5cf8f57d9d76c03c619209
BabimKhanal <bgoogly0@gmail.com>
2023-12-29 22:10:38 +0545

    This is a new commit for what I originally planned to be amended

--- actions/actions.py
+++ actions/actions.py
@@ -28,6 +28,10 @@
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+
+
 
 class ActionCheckBalance(Action):
     def name(self) -> Text:
@@ -38,19 +42,31 @@ class ActionCheckBalance(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
         account_number = tracker.get_slot("account_number")
+<<<<<<< HEAD
         #database dictionary 
+=======
+        phone = tracker.get_slot("phone")
+        user_name = tracker.get_slot("name")
+        
+        
+#database with Dict
+>>>>>>> 18ee3a7 (update)
         database = {
-            "1234567890": {"name": "Alice", "balance": 1000},
-            "0987654321": {"name": "Bob", "balance": 2000}
-            # Add more user information...
+            "1234567890": {"name": "Babim", "balance": 1000 , "phone": 9846863138},
+            "0987654321": {"name": "Bishal", "balance": 2000, "phone": 9804109276},
         }
-
+        
+#validation 
         if account_number in database:
             user_info = database[account_number]
             user_name = user_info.get("name")
             balance = user_info.get("balance")
-            dispatcher.utter_message(f"Dear {user_name}, your account number {account_number} has a balance of Rs. {balance}")
+            phone = user_info.get("phone")
+            dispatcher.utter_message(f"Dear {user_name}, \n your account number {account_number} \n has a balance of Rs. {balance}\n Your registered number is {phone}")
         else:
             dispatcher.utter_message("User not found in the database")
 
         return []
+    
+
+
